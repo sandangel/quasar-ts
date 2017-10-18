9a454c27a871ad68fa8108a1e5016f0fa30442e5
sandangel <vinhsannguyen91@gmail.com>
2017-10-18 11:36:03 +0900

    Template for Quasar and TS

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- Don't drop "q-app" class -->
+  <div id="q-app">
+    <header></header>
+    <main>
+      <img src="~assets/quasar-logo-full.svg"
+           alt="Quasar PWA">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+/*
+ * Root component
+ */
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+
+}
+</script>
+
+<style lang="stylus">
+@import '~variables';
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 50px;
+  background-color: $primary;
+}
+</style>
---
+++ templates/page.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- if you want automatic padding use "layout-padding" class -->
+  <div class="layout-padding">
+    <!-- your content -->
+  </div>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class Hello extends Vue { }
+</script>
+
+<style>
+
+</style>
